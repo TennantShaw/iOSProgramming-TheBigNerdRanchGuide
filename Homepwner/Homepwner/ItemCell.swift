15ba39b2e9303a3ee4c5cf0eed031b6aac4811d8
Tennant Shaw <tennantshaw@Macs-MBP.lan>
2017-05-15 11:49:04 -0400

    finished chapter 12

---
+++ Homepwner/Homepwner/ItemCell.swift
@@ -0,0 +1,23 @@
+//
+//  ItemCell.swift
+//  Homepwner
+//
+//  Created by Tennant Shaw on 5/15/17.
+//  Copyright © 2017 Tennant Shaw. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var serialNumberLabel: UILabel!
+    @IBOutlet var valueLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        nameLabel.adjustsFontForContentSizeCategory = true
+        serialNumberLabel.adjustsFontForContentSizeCategory = true
+        valueLabel.adjustsFontForContentSizeCategory = true
+    }
+}
