db704fad3bb20597609ca78d7445753de94801a9
Tennant Shaw <tennantshaw@Macs-MBP.attlocal.net>
2017-05-16 20:29:41 -0400

    finished chapter 18

---
+++ TouchTracker/TouchTracker/Line.swift
@@ -0,0 +1,15 @@
+//
+//  Line.swift
+//  TouchTracker
+//
+//  Created by Tennant Shaw on 5/16/17.
+//  Copyright © 2017 Tennant Shaw. All rights reserved.
+//
+
+import Foundation
+import CoreGraphics
+
+struct Line {
+    var begin = CGPoint.zero
+    var end = CGPoint.zero
+}
